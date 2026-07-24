9bea14e10acda9dd7f8414e0b6ae7a03f3f068c7
lightzane <bhest.pat@gmail.com>
2026-07-24 17:04:14 +0800

    add docker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Useful for self-hosting a Docker container
 };
 
 export default nextConfig;
